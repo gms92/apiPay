ae2c5cdff814945da23a54f4d873cd128be97184
guilherme.maia <guilherme.maia@creditas.com>
2024-12-25 02:52:17 -0300

    test: add initial application test cases

---
+++ src/test/kotlin/com/example/apiPay/ApiPayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apiPay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiPayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
